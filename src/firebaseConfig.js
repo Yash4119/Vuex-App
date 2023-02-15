28268864788370b05ed238b2d24bccc76501b510
yash ambekar <yashamb444@gmail.com>
2023-02-15 18:28:52 +0530

    firestore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,9 +12,38 @@ export const firebaseConfig = {
   projectId: "vuex-app-5e3ec",
   storageBucket: "vuex-app-5e3ec.appspot.com",
   messagingSenderId: "834412071430",
-  appId: "1:834412071430:web:29f9a085e64fbdcba19187"
+  appId: "1:834412071430:web:29f9a085e64fbdcba19187",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app
+const db = getFirestore(app);
+
+const putData = async() => {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Yash",
+      last: "Coder",
+      born: 2000
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+const getData = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data().first}`);
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+putData();
+getData();
+
+export default app;
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView2 from '../views/HomeView2.vue'
 import CounterView from "../components/CounterView.vue"
 import EmployeesView from "../components/EmployeesView.vue"
 import UserList from "../components/UserList.vue"
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView2
   },
   {
     path: '/counter',
